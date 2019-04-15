97f9fe4b93808d2a781718399ebe70feed40e426
MLobo1997 <miguelobo1997@gmail.com>
2019-04-15 21:47:26 +0100

    Started optimizing SVR

--- utils/ContinuityImputer.py
+++ utils/ContinuityImputer.py
@@ -10,8 +10,9 @@ class ContinuityImputer(BaseEstimator, TransformerMixin):
         X_iq = X[X['city'] == 'iq']
         X_sj = X[X['city'] == 'sj']
 
-        self.medians_iq = {attr: np.nanmedian(X_iq[attr]) for attr in self.attributes}
-        self.medians_sj = {attr: np.nanmedian(X_sj[attr]) for attr in self.attributes}
+        medians_iq = {attr: np.nanmedian(X_iq[attr]) for attr in self.attributes}
+        medians_sj = {attr: np.nanmedian(X_sj[attr]) for attr in self.attributes}
+        self.last_values = {'sj': medians_sj, 'iq': medians_iq}
 
         return self
     
@@ -20,14 +21,13 @@ class ContinuityImputer(BaseEstimator, TransformerMixin):
             X = X.copy()
 
         for attr in self.attributes:
-            last_values = {'sj': self.medians_sj[attr], 'iq': self.medians_iq[attr]}
             r = []
             for _, curr in X.iterrows():
                 city = curr['city']
                 val = curr[attr]
                 if val is not None and not np.isnan(val):
-                    last_values[city] = val
-                r.append(last_values[city])
+                    self.last_values[city][attr] = val
+                r.append(self.last_values[city][attr])
             X[attr] = r
 
         return X
