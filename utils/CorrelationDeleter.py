b2cae277f9cd2c9a36f3e731a0745b31629d3940
MLobo1997 <miguelobo1997@gmail.com>
2019-06-02 16:31:35 +0100

    correlation deleter

---
+++ utils/CorrelationDeleter.py
@@ -0,0 +1,20 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+
+class CorrelationDeleter(BaseEstimator, TransformerMixin):
+    def __init__(self, threshold_corr=0.9):
+        self.threshold_corr=threshold_corr
+    
+    def fit(self, X, y=None):
+        corr_matrix = np.corrcoef(X, rowvar=False)
+        self.drop_cols = []
+        n_cols = corr_matrix.shape[1]
+        for i in range(n_cols - 1):
+            for j in range(i+1,n_cols):
+                if j not in self.drop_cols:
+                    val = corr_matrix[i, j]
+                    if abs(val) >= self.threshold_corr:
+                        self.drop_cols.append(j) 
+    
+    def transform(self, X):
+        return np.delete(X, self.drop_cols, axis=1)
--- utils/OurPipeline.py
+++ utils/OurPipeline.py
@@ -5,15 +5,23 @@ from utils.ContinuityImputer import ContinuityImputer
 from utils.DataFrameDropper import DataFrameDropper
 from utils.LastWeeks import LastWeeks
 from utils.LastInfected import LastInfected
+from utils.CorrelationDeleter import CorrelationDeleter
+from sklearn.feature_selection import SelectKBest, f_regression
 
-def create_pipeline(attr, n_weeks=None, n_weeks_infected=None, estimator_optimizer=None, pca=None, add_noise=False, noise_mean=None, noise_std=None, n_non_train=4):
+def create_pipeline(attr, n_weeks=None, n_weeks_infected=None, estimator_optimizer=None, pca=None, add_noise=False, noise_mean=None, noise_std=None, n_non_train=4, k_best=None, threshold_corr=None):
 
     l_infected = None
     l_weeks = None
+    sel_k_best = None
+    corr_del = None
     if n_weeks_infected is not None and n_weeks_infected > 0:
         l_infected = LastInfected(weeks=n_weeks_infected, add_noise=add_noise, noise_mean=noise_mean, noise_std=noise_std)
     if n_weeks is not None and n_weeks > 0:
         l_weeks = LastWeeks(attributes=attr[n_non_train:], weeks=n_weeks)
+    if k_best is not None:
+        sel_k_best = SelectKBest(f_regression, k_best)
+    if threshold_corr is not None:
+        corr_del = CorrelationDeleter(threshold_corr=threshold_corr)
 
     return Pipeline([
         ('imputer', ContinuityImputer(attributes=attr[n_non_train:])),
@@ -22,6 +30,8 @@ def create_pipeline(attr, n_weeks=None, n_weeks_infected=None, estimator_optimiz
         ('dataframe_dropper', DataFrameDropper(attribute_names=attr[:n_non_train])),
         ('scaler', StandardScaler()),
         ('pca', pca),
+        ('sel_k_best', sel_k_best),
+        ('corr_del', corr_del),
         ('est_opt', estimator_optimizer),
     ]
 )
